7c21d1e4865268f673f352dd2790940e74ed8570
miketheo423 <miketheo423@gmail.com>
2017-09-23 16:34:35 -0600

    Fixes middle column win logic

--- js/app.js
+++ js/app.js
@@ -13,9 +13,7 @@ $(function(){
 //If the current state of the game is "false"- then it is Player One's Turn
 //If current state is "true" then it is Player Two's Turn
 
-
 //Variables for spots
-
 var cell1 = $('#cell1');
 var cell2 = $('#cell2');
 var cell3 = $('#cell3');
@@ -25,9 +23,6 @@ var cell6 = $('#cell6');
 var cell7 = $('#cell7');
 var cell8 = $('#cell8');
 var cell9 = $('#cell9');
-
-
-
 var turns = 0;
 
 $('#gameBoard tbody').on('click', 'td', function(){
@@ -36,42 +31,34 @@ $('#gameBoard tbody').on('click', 'td', function(){
 	} else {
 		$(this).text("O").addClass('o');
 	}
-
 	turns++;
 	if (turns > 8){
 		alert("Its a tie!");
 		location.reload();
 	}
-
 	if(cell1.hasClass('x') && cell2.hasClass('x') && cell3.hasClass('x') ||
 		cell4.hasClass('x') && cell5.hasClass('x') && cell6.hasClass('x') ||
 		cell7.hasClass('x') && cell8.hasClass('x') && cell9.hasClass('x') ||
 		cell1.hasClass('x') && cell5.hasClass('x') && cell9.hasClass('x') ||
 		cell7.hasClass('x') && cell5.hasClass('x') && cell3.hasClass('x') ||
 		cell1.hasClass('x') && cell4.hasClass('x') && cell7.hasClass('x') ||
-		cell2.hasClass('x') && cell6.hasClass('x') && cell8.hasClass('x') ||
+		cell2.hasClass('x') && cell5.hasClass('x') && cell8.hasClass('x') ||
 		cell3.hasClass('x') && cell6.hasClass('x') && cell9.hasClass('x')) {
 		alert("Player X Has Won!");
 		location.reload();
 	}
-
 	if(cell1.hasClass('o') && cell2.hasClass('o') && cell3.hasClass('o') ||
 		cell4.hasClass('o') && cell5.hasClass('o') && cell6.hasClass('o') ||
 		cell7.hasClass('o') && cell8.hasClass('o') && cell9.hasClass('o') ||
 		cell1.hasClass('o') && cell5.hasClass('o') && cell9.hasClass('o') ||
 		cell7.hasClass('o') && cell5.hasClass('o') && cell3.hasClass('o') ||
 		cell1.hasClass('o') && cell4.hasClass('o') && cell7.hasClass('o') ||
-		cell2.hasClass('o') && cell6.hasClass('o') && cell8.hasClass('o') ||
+		cell2.hasClass('o') && cell5.hasClass('o') && cell8.hasClass('o') ||
 		cell3.hasClass('o') && cell6.hasClass('o') && cell9.hasClass('o')) {
 		alert("Player O Has Won!");
 		location.reload();
 	}
-
 });
-		
-
-
-
-
+	
 
 });
